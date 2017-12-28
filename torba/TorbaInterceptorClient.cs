ed7e255909597905fca15ea0967fb58843feed53
DHC\kozakevych <oleg.kozakevych@dhc-vision.com>
2017-12-28 15:20:04 +0100

    transport added

--- torba/TorbaInterceptorClient.cs
+++ torba/TorbaInterceptorClient.cs
@@ -1,12 +1,20 @@
-﻿using Castle.DynamicProxy;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.DynamicProxy;
 
 namespace torba
 {
-    public class TorbaInterceptorClient: IInterceptor
-    {
-       public void Intercept(IInvocation invocation)
-       {
-          invocation.Proceed();
-       }
-    }
+   public class TorbaInterceptorClient: IInterceptor
+   {
+      public void Intercept(IInvocation invocation)
+      {
+         string className = invocation.InvocationTarget.GetType().Name;
+         List<object> args = invocation.Arguments.ToList();
+         string retType = invocation.Method.ReturnType.ToString();
+         Console.Out.WriteLine($"class: {className}, argumens: {string.Join(",", args)}, return type: {retType}");
+         // with return type methods, NPE is thrown without Proceed
+         invocation.Proceed();
+      }
+   }
 }
