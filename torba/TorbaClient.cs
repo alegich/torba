394bc8ec40fc86219aa285c128dd7550ab3f7f54
DHC\kozakevych <oleg.kozakevych@dhc-vision.com>
2017-12-28 14:52:39 +0100

    rename

---
+++ torba/TorbaClient.cs
@@ -0,0 +1,16 @@
+﻿using Castle.DynamicProxy;
+
+namespace torba
+{
+   public class TorbaClient<T> where T: class
+   {
+      private static readonly ProxyGenerator generator = new ProxyGenerator();
+
+      public T Proxy { get; private set; }
+
+      public TorbaClient(T proxied)
+      {
+         Proxy = (T)generator.CreateClassProxyWithTarget(typeof (T), proxied, new TorbaInterceptorClient());
+      }
+   }
+}
