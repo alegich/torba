f9108bfedf253f8846e96c9f43f4e9e5a3a7f9d6
alegich <31540570+alegich@users.noreply.github.com>
2017-12-28 14:46:16 +0100

    Add files via upload

---
+++ torba/TorbaInterceptorClient.cs
@@ -0,0 +1,12 @@
+﻿using Castle.DynamicProxy;
+
+namespace torba
+{
+    public class TorbaInterceptorClient: IInterceptor
+    {
+       public void Intercept(IInvocation invocation)
+       {
+          invocation.Proceed();
+       }
+    }
+}
