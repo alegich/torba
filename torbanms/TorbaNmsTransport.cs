5f517fcbad21962216eab9de6f9f03020f3e4de6
DHC\kozakevych <oleg.kozakevych@dhc-vision.com>
2018-01-05 18:37:39 +0100

    NMS transport stub library

---
+++ torbanms/TorbaNmsTransport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using torba;
+
+namespace torbanms
+{
+   public class TorbaNmsTransport : ITorbaTransport
+   {
+      public ITorbaResponse SendRequest(ITorbaRequest request)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
